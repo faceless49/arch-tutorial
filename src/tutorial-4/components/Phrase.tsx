ae68284318bb01e42b31e73671acc6991d0df682
faceless49 <digital.zeus@mail.ru>
2021-10-17 21:29:58 +0300

    Fix request

--- src/tutorial-4/components/Phrase.tsx
+++ src/tutorial-4/components/Phrase.tsx
@@ -5,14 +5,13 @@ type PhrasePropsType = {
 export const Phrase = (props: PhrasePropsType) => {
   return (
     <div className="list">
-      {props.phrases &&
-        props.phrases.map((p, i) => {
-          return (
-            <div className="block" key={i}>
-              <h3>{p}</h3>
-            </div>
-          );
-        })}
+      {props.phrases?.map((p, i) => {
+        return (
+          <div className="block" key={i}>
+            <h3>{p}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 };
