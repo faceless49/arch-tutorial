f998bed9f5f929ce4c58d409fbf2c3a6b4c97f87
faceless49 <digital.zeus@mail.ru>
2021-10-24 18:16:12 +0300

    Сделал ДЗ-6

---
+++ src/tutorial-6/components/Article.tsx
@@ -0,0 +1,21 @@
+type ArticlePropsType = {
+  id: string;
+};
+export const Article = (props: ArticlePropsType) => {
+  return (
+    <div>
+      <div>
+        <h1>Статья №{props.id}</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          distinctio fuga animi aliquam sit id veritatis, doloribus ducimus
+          quas, dignissimos non minima quia amet possimus? Impedit nemo ducimus
+          fuga rem!
+        </p>
+        <a href="/">
+          <button>Назад</button>
+        </a>
+      </div>
+    </div>
+  );
+};
